523c65bc51f28629d7b57c313413bcda299928e4
Andrew Mather <Andy-mather@live.co.uk>
2018-11-06 11:07:48 +0000

    initial commit

---
+++ stopandsearch/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+	state = {
+		seaches: []
+	};
+	render() {
+		return <div className="App" />;
+	}
+}
+
+export default App;
